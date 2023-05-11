78bfc672aaf24b4d85d78b95615d8cfe490a92f7
gerrygeop <gerryja28@gmail.com>
2023-05-11 18:38:03 +0800

    user model

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,20 +19,11 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'nim' => fake()->unique()->nik(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'faculty' => fake()->numberBetween(1, 13),
+            'department' => fake()->numberBetween(1, 50),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('nim')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->integer('faculty');
+            $table->integer('department');
+            $table->string('token')->nullable();
+            $table->dateTime('token_expires_at')->nullable();
             $table->timestamps();
         });
     }
