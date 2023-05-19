ef2e598790c7ccf5277654caec72ab9c4e4ca324
gerrygeop <gerryja28@gmail.com>
2023-05-19 08:00:34 +0800

    paslon routing

--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Dapur\Auth\AdminPasswordController;
 use App\Http\Controllers\Dapur\Auth\AdminAuthenticatedSessionController;
 use App\Http\Controllers\Dapur\Auth\RegisteredAdminController;
+use App\Http\Controllers\PaslonController;
 use App\Http\Controllers\PemiraController;
 use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
@@ -29,6 +30,13 @@ Route::prefix('d')->name('d.')->group(function () {
         // Pemira
         Route::resource('pemira', PemiraController::class);
         Route::patch('pemira/switchable/{pemira}', [PemiraController::class, 'switchable'])->name('pemira.switchable');
+
+        // Paslon
+        Route::prefix('pemira')->name('pemira.')->group(function () {
+            Route::get('paslon/create/{pemira}', [PaslonController::class, 'create'])->name('paslon.create');
+            Route::post('paslon/{pemira}', [PaslonController::class, 'store'])->name('paslon.store');
+            Route::resource('paslon', PaslonController::class)->except('create', 'store', 'index');
+        });
 
         // Roles & Permissions
         Route::resource('roles', RoleController::class)->except(['show', 'create', 'edit']);
