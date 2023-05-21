e5aeb2fd61cd1a6b2468411650dde253a8118b88
gerrygeop <gerryja28@gmail.com>
2023-05-22 06:06:12 +0800

    admin index controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Admin;
+use App\Models\Role;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class AdminController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('can:cooking');
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Response
+    {
+        return Inertia::render('Dapur/Panitia/Index', [
+            'panitiaList' => Admin::whereNotIn('id', [Auth::guard('admin')->id()])->with('roles')->get(),
+            'roles' => Role::all()
+        ]);
+    }
 }
