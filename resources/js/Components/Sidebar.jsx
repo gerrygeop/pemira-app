a4a7bd978b1d6d0dbd9ad4c3860a252345ba7a59
gerrygeop <gerryja28@gmail.com>
2023-05-24 02:19:27 +0800

    delete panitia menu from sidebar

--- resources/js/Components/Sidebar.jsx
+++ resources/js/Components/Sidebar.jsx
@@ -39,16 +39,6 @@ export default function Sidebar({ can }) {
 
                     {can.includes("cooking") && (
                         <SideLink
-                            href={route("d.panitia.index")}
-                            active={route().current("d.panitia.*")}
-                        >
-                            <IconBuildingBank />
-                            <span className="tracking-wide">Panitia</span>
-                        </SideLink>
-                    )}
-
-                    {can.includes("cooking") && (
-                        <SideLink
                             href={route("d.roles.index")}
                             active={route().current("d.roles.*")}
                         >
