1bbe0db89f7e84325aaf6b265f4590dae15f5b08
gerrygeop <gerryja28@gmail.com>
2023-06-06 15:40:47 +0800

    update layout

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -4,7 +4,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 export default function Authenticated({ header, children }) {
     return (
-        <div className="min-h-screen bg-[#383a59] dark:bg-[#f0f2f5]">
+        <div className="min-h-screen bg-[#f0f2f5]">
             <Navbar header={header} />
 
             <main>{children}</main>
