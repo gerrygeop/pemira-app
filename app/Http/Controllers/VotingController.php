6a76ba764c6406b623ad0e3d775c89c3f2923960
gerrygeop <gerryja28@gmail.com>
2023-06-10 10:02:49 +0800

    voting controller

---
+++ app/Http/Controllers/VotingController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pemira;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class VotingController extends Controller
+{
+    public function index(): Response
+    {
+        $admin = Auth::guard('admin')->id();
+        $pemira = Pemira::where('creator_id', $admin)->orWhereRelation('admins', 'admin_id', $admin)->get();
+
+        return Inertia::render('Dapur/Voting/Index');
+    }
+}
